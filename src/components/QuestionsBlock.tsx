ade3d60418562fd0e07be68a944a5c973bd326bb
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-24 16:20:24 +0100

    Display 4 options for an answer in all of the questions

--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Content } from '../../interfaces'
+import { Content, Question } from '../../interfaces'
+import { QuestionBlock } from './QuestionBlock'
 
 export const QuestionsBlock = ({ quizItem }: {
   quizItem: Content
@@ -7,6 +8,14 @@ export const QuestionsBlock = ({ quizItem }: {
   return (
     <div>
       <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+      <div className='questions-container'>
+        {quizItem?.questions.map((question: Question, _index) => (
+          <QuestionBlock
+            key={_index}
+            question={question}
+          />
+        ))}
+      </div>
     </div>
   )
 }
